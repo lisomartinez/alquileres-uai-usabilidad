a0f256838e621ccd9bddb5b03672c4715f789c26
Lisandro Martinez <lisandromartinez@gmail.com>
2020-12-05 16:55:57 -0300

    fallback to true

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,9 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'server',
   buildDir: 'dist',
-
+  generate: {
+    fallback: true
+  },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'alquileres-uai-usabilidad',
